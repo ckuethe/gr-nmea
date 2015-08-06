a944fed52b9d7cfeefd144a49cd2a1ceb417fac5
Chris Kuethe <chris.kuethe+github@gmail.com>
2015-08-05 18:05:30 -0700

    add a selector for gpsd native mode

--- python/nmea_parser_core.py
+++ python/nmea_parser_core.py
@@ -22,6 +22,7 @@
 
 import pynmea2
 import pmt
+import json
 
 from pmt import string_to_symbol as PSTR
 from pmt import from_double as PDBL
@@ -29,6 +30,23 @@ from pmt import dict_add as PMT_ADD
 
 from datetime import datetime as dt
 from datetime import date as dtdate
+
+def gpsd_parser_core(self, gpsd_str):
+	G = json.loads(gpsd_str)
+	if 'TPV' not in G['class']:
+		return
+
+	D = pmt.make_dict()
+	D = PMT_ADD(D, PSTR("id"), PSTR(str(G['tag'])))
+	D = PMT_ADD(D, PSTR("protocol"), PSTR('gpsd'))
+	D = PMT_ADD(D, PSTR("host_time"), PSTR(str(self.host_time)))
+	D = PMT_ADD(D, PSTR("gps_time"), PSTR(str(G['time'])))
+	D = PMT_ADD(D, PSTR("longitude"), PDBL(G['lon']))
+	D = PMT_ADD(D, PSTR("latitude"), PDBL(G['lat']))
+	D = PMT_ADD(D, PSTR("altitude"), PDBL(G['alt']))
+	D = PMT_ADD(D, PSTR("speed"), PDBL(G['speed']))
+	D = PMT_ADD(D, PSTR("track"), PDBL(G['track']))
+	self.message_port_pub(pmt.intern('gps_msg'), pmt.cons(D, pmt.PMT_NIL))
 
 def nmea_parser_core(self, nmea_str):
 	fixobj = pynmea2.parse(nmea_str)
@@ -39,6 +57,7 @@ def nmea_parser_core(self, nmea_str):
 
 	D = pmt.make_dict()
 	D = PMT_ADD(D, PSTR("id"), PSTR(nmea_id))
+	D = PMT_ADD(D, PSTR("protocol"), PSTR('nmea'))
 	D = PMT_ADD(D, PSTR("host_time"), PSTR(str(self.host_time)))
 
 	try:
@@ -60,29 +79,22 @@ def nmea_parser_core(self, nmea_str):
 		pass
 
 	try:
-		D = PMT_ADD(D, PSTR("true_track"), PDBL(fixobj.true_track))
+		D = PMT_ADD(D, PSTR("track"), PDBL(fixobj.true_track))
 	except AttributeError:
 		pass
 
 	try:
 		if fixobj.spd_over_grnd_kmph is not None:
-			D = PMT_ADD(D, PSTR("speed_kmh"), PDBL(fixobj.spd_over_grnd_kmph))
+			D = PMT_ADD(D, PSTR("speed"), PDBL(fixobj.spd_over_grnd_kmph))
 	except AttributeError:
 		pass
 
 	try:
 		self.nmea_time = dt.combine(fixobj.datestamp, fixobj.timestamp)
-		D = PMT_ADD(D, PSTR("nmea_time"), PSTR(str(self.nmea_time)))
+		D = PMT_ADD(D, PSTR("gps_time"), PSTR(str(self.nmea_time)))
 	except AttributeError:
 		pass
 
 	# Send the message
 	if D:
 		self.message_port_pub(pmt.intern('gps_msg'), pmt.cons(D, pmt.PMT_NIL))
-	'''
-	except pynmea2.nmea.ParseError:
-		# nmea parse failure is forseeable, but is deemed to not be fatal
-		pass
-	except AttributeError:
-		pass
-	'''
